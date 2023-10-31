02556b835f77d4f6fd6969685cb7c134225b57b5
jo <145546435+georgetj2026@users.noreply.github.com>
2023-10-31 14:17:01 +0530

    Add files via upload

---
+++ transpose.java
@@ -0,0 +1,38 @@
+package javalab;
+import java.util.Scanner;
+
+public class transpose {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int arr1[][], arr2[][], row, column;
+        System.out.print("Enter the number of rows : ");
+        row = input.nextInt();
+        System.out.print("Enter the number of columns : ");
+        column = input.nextInt();
+        System.out.println();
+        arr1 = new int[row][column];
+        arr2 = new int[column][row];
+        for(int i=0; i<row; i++){
+            for(int j=0; j<column; j++){
+                System.out.print("Enter the value of arr1["+i+"]["+j+"] : ");
+                arr1[i][j] = input.nextInt();
+            } }
+        System.out.println();
+        for(int i=0; i<column; i++){
+            for(int j=0; j<row; j++){
+                arr2[i][j] = arr1[j][i];
+            }}
+        System.out.println("arr1 is");
+        for(int i=0; i<row; i++){
+            for(int j=0; j<column; j++){
+                System.out.print(arr1[i][j] + " ");
+            }   
+            System.out.println();  }
+        System.out.println();
+        System.out.println("Transpose of arr1 is");
+        for(int i=0; i<column; i++){
+            for(int j=0; j<row; j++){
+               System.out.print(arr2[i][j] + " ");
+            }
+            System.out.println();
+        }}}
