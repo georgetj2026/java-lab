276e0c5337a8b6f04131bbd95f871b0b0fcac857
jo <145546435+georgetj2026@users.noreply.github.com>
2023-11-17 14:30:58 +0530

    Add files via upload

---
+++ ExceptionTrycatch.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class ExceptionTrycatch {
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	char ans = 'y';
+	while(ans == 'y') {
+		System.out.println("Program to Perform Division");
+		System.out.print("Enter the Number-1:");
+		int num1 = sc.nextInt();
+		System.out.print("Enter the Number-2:");
+		int num2 = sc.nextInt();
+		try {
+			int result = num1/num2;
+			System.out.println("Resut="+result);
+		}
+		catch (Exception e) {
+			System.out.println("/by zero");
+			
+		}
+		finally {
+			System.out.println("End of Operation");
+		}
+		System.out.println("Do you want to continue y or n");
+		ans = sc.next().charAt(0);
+
+
+		
+	}
+	
+}
+
+}
